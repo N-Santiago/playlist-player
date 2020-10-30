59cc41a5aaad6a0ba6af747813d5ee02082b29d8
N-Santiago <norbertosantiago82@gmail.com>
2020-10-29 21:14:56 -0500

    search controller

--- playlist-player-backend/app/controllers/api/v1/search_controller.rb
+++ playlist-player-backend/app/controllers/api/v1/search_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SearchController < ApplicationController
 
     def index
       spotify_url = "https://api.spotify.com/v1/"
-      track = params[:name]
+      track = params[:title]
       searchURL = "#{spotify_url}search?q=#{track}&type=track"
     end 
 
