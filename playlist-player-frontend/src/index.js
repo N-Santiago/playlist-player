ec1ba5015baca744fae0727feee4588d55497fe5
N-Santiago <norbertosantiago82@gmail.com>
2020-10-30 09:37:16 -0500

    fetch code

--- playlist-player-frontend/src/index.js
+++ playlist-player-frontend/src/index.js
@@ -1,1 +1,20 @@
-//test 
+const BASE_URL = "http://localhost:3000"
+const TRAINERS_URL = `${BASE_URL}/tracks`
+
+document.addEventListener('DOMContentLoaded', () => {
+    getTracks()
+  })
+  
+  const main = () => {
+    return document.querySelector('main')
+  }
+  
+  const getTrainers = () => {
+    fetch('http://localhost:3000/tracks')
+    .then(r => r.json())
+    .then(tracks => renderTracks(tracks))
+  }
+
+  const renderTracks = (tracksData) => {
+    tracksData.forEach(track => document.log(track))
+  }
