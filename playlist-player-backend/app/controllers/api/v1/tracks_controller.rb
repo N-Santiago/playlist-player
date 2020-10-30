6319e87b6c4e04159091bc42c2213d398d05f7eb
N-Santiago <norbertosantiago82@gmail.com>
2020-10-29 21:50:06 -0500

    tracks controller index and show

--- playlist-player-backend/app/controllers/api/v1/tracks_controller.rb
+++ playlist-player-backend/app/controllers/api/v1/tracks_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::TracksController < ApplicationController
+
+    def index
+        tracks = Track.all
+        render json: tracks
+    end 
+
+    def show
+        track = track.find_by(spotify_id: params[:spotify_id])
+        render json: track
+    end 
+    
 end
