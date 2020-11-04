b142d183ff4800825a756f6f2c968fc953a410c4
N-Santiago <norbertosantiago82@gmail.com>
2020-11-03 21:52:36 -0600

    track form in js file

--- playlist-player-frontend/src/index.js
+++ playlist-player-frontend/src/index.js
@@ -2,12 +2,21 @@ const BASE_URL = 'https://localhost:3000'
 const TRACKS_URL = `${BASE_URL}/api/v1/tracks/`
 
 window.addEventListener('load', () => {
+    addTrackForm()
     getTracks()
 })
 
 const main = () => {
     return document.querySelector('main')
 }
+
+// function clickToLinks() {
+//     let playerLinks = document.querySelectorAll('.card-header a')
+//     playerLinks.forEach(track => {
+//         player.addEventListener('click', getTracks)
+//     })
+//     document.querySelectorAll('#add-track').forEach(track => track.addEventListener('click', addTrackForm))
+// }
 
 const getTracks = () => {
     fetch('http://localhost:3000/api/v1/tracks')
@@ -28,3 +37,50 @@ const renderTrackCard = (trackObj) => {
     `
     main().appendChild(trackCard)
 }
+
+function addTrackForm() {
+    let trackForm = document.getElementById('track-form')
+    let html = `
+        <form>
+            <label>Title:</label>
+            <input type="text" id="title">
+            <label>Artist:</label>
+            <input type="text" id="artist">
+            <label>Genre:</label>
+            <input type="text" id="genre">
+            <input type="submit">
+        </form>
+    `
+    trackForm.innerHTML = html 
+    document.querySelector('form').addEventListener('submit', createTrack)
+}
+
+function clearTrackForm() {
+    let trackFormDiv = document.getElementById('track-form')
+    trackFormDiv.innerHTML = ""
+}
+
+function createTrack() {
+    event.preventDefault()
+    const track = {
+        Title: document.getElementById('title').value,
+        Artist: document.getElementById('artist').value,
+        Genre: document.getElementById('genre').value,
+    }
+    fetch(BASE_URL+"/api/v1/tracks", {
+        method: "POST",
+        body: JSON.stringify(track),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }) 
+    .then(resp => resp.json())
+    .then(track => {
+    renderTrackCard(track)
+    // attachClickToLinks()
+    clearConcertForm()
+    })
+}
+
+    
