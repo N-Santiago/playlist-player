9b9a792d02635887365c742096df4995d4f99e6c
N-Santiago <norbertosantiago82@gmail.com>
2020-11-19 08:53:20 -0600

    playlist display issue is fixed

--- playlist-player-frontend/src/index.js
+++ playlist-player-frontend/src/index.js
@@ -4,13 +4,23 @@ const PLAYLISTS_URL = `${BASE_URL}/playlists/`
 
 window.addEventListener('load', () => {
     getPlaylists()
-    // addTrackForm()
+    addTrackForm()
     // renderTrack()
 })
 
 const main = () => {
     return document.querySelector('main')
 }
+
+// function clickToLinks() {
+//         let playerLinks = document.querySelectorAll('playlist-container')
+//         playerLinks.forEach(playlists => {
+//             player.addEventListener('click', getplaylists())
+//         })
+//         // document.querySelectorAll('#add-track').forEach(track => track.addEventListener('click', addTrackForm))
+//         // document.querySelectorAll('#delete-track').forEach(track => track.addEventListener('click', removeTrackButton))             
+//     }
+    
 
 function getPlaylists() {
     // clearForm()
@@ -20,34 +30,13 @@ function getPlaylists() {
     .then(playlists => {
         playlists.map(playlist => { 
             // console.log(playlist) 
-            debugger 
             let p = new Playlist(playlist)
-            main.innerHTML += p.renderPlaylist() 
+            main().innerHTML += p.renderPlaylist() 
             p.renderTracks()
         })
     })
     // attachClickToLinks()
 }
-
-// function displayPlaylist(){
-//     // clearForm()
-//     let id = event.target.dataset.id 
-//     main.innerHTML = ""
-//     fetch(PLAYLISTS_URL+id)
-//     .then(resp => resp.json())
-//     .then(playlist => {
-//         main.innerHTML += `<div class="playlist-container id="playlist-${playlist.id}">
-//         <div class="playlist-header">
-//             <a href='#' data-id='${playlist.id}' class="my-0 font-weight-normal">${playlist.name}</a><br>
-//             <a href='#' data-id='${playlist.id}' class="my-0 font-weight-normal">${playlist.description}</a><br>
-//             <button class="playlist-btn" id="add-track" data-id="${playlist.id}">Add Track</button>
-//         </div>    
-//         <div class="playlist-body">
-//             <ul id="tracks-list" class="tracks-list"></ul>
-//         </div>
-//     </div>`                        
-//     })
-// }
 
 function addTrackForm() {
     let trackForm = document.getElementById('track-form')
--- playlist-player-frontend/src/playlist.js
+++ playlist-player-frontend/src/playlist.js
@@ -1,18 +1,19 @@
 class Playlist {
+    static all = []
+
     constructor(playlist) {
         this.id = playlist.id
         this.name = playlist.name
         this.description = playlist.description
         this.tracks = playlist.tracks
-        // Playlist.array.push(this)
+        Playlist.all.push(this)
     }
 
     renderPlaylist() { 
         return `<div class="playlist-container" id="playlist-${this.id}">
                     <div class="playlist-header">
-                        <a href='#' data-id='${this.id}' class="my-0 font-weight-normal">${this.name}</a><br>
-                        <a href='#' data-id='${this.id}' class="my-0 font-weight-normal">${this.description}</a><br>
-                        <button class="playlist-btn" id="add-track" data-id="${this.id}">Add Track</button>
+                        <h3><a data-id='${this.id}' class="my-0 font-weight-normal">${this.name}</a></h3>
+                        <h4><a data-id='${this.id}' class="my-0 font-weight-normal">${this.description}</a><h4>
                     </div>    
                     <div class="playlist-body">
                         <ul id="tracks-list" class="tracks-list"></ul>
@@ -22,10 +23,11 @@ class Playlist {
 
     renderTracks() {
         // let trackList = document.querySelector(`#playlist-${this.id} #tracks-list`) 
+        // debugger
         this.tracks.forEach(track => {
-            main.innerHTML += `
+            main().innerHTML += `
             <li id="track-${track.id}">
-            <small>${this.title} - ${this.artist} - Genre: ${this.genre}</small><button class="delete-btn" data-action="delete" id="delete-btn"> X </button></li>`
+            ${track.title} - ${track.artist} - Genre: ${track.genre}</small><button class="delete-btn" data-action="delete" id="delete-btn"> X </button></li>`
         })
     }
 
